ec8dd5fe0b285650b37c5d4e8f6f4de520f5ded3
Joaotbb <joaotbarros9@gmail.com>
2022-10-24 21:55:07 +0100

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+let btn =document.getElementById("btn")
+let inputName =document.getElementById("input-name")
+let inputLastName =document.getElementById("input-last-name")   
+let inputemail =document.getElementById("input-email")
+let inputpassword =document.getElementById("input-password")
+
+
+btn.addEventListener("click",function(){
+// if(inputName.value=="") {
+//     inputName.classList.remove("hiden")
+
+// }   else if(inputLastName.value=="") {
+//         console.log("lastname")
+        
+
+// }   else if(inputemail.value==""){
+//         console.log("emaiL")
+
+// }   else if(inputpassword.value=="") {
+//         console.log("password")
+
+// }   else {
+//     console.log("esta tudo preenchdo")
+// }
+
+})
+
